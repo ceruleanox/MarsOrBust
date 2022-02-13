beca9cf5384b40184b09fff593aa6207a3d3c7a5
Anita Yip <ceruleanox@gmail.com>
2022-02-12 21:22:29 -0500

    calc merged to main screen; video background added to splash

--- people.js
+++ people.js
@@ -1,10 +1,15 @@
-document.getElementById("btnSub").addEventListener("click", processInputpassengers);
+
+//document.getElementById("adults").addEventListener("click", processInputpassengers);
+//document.getElementById("btnSub").addEventListener("click", processInputpassengers);
 function processInputpassengers()
 {
+
+numAdults = document.getElementById("adults").value;
+
     var adultsval =  parseInt(adults.value);
-    console.log(adultsval);
+    console.log("adults: "+ adultsval);
     var childrensval =  parseInt(children.value);
-    console.log(childrensval);
+    console.log("children: " + childrensval);
     if (adultsval == 0 && childrensval != 0)
     {
         window.alert("Are you sure? There are only children on this trip")
